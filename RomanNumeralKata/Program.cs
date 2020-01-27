10ea941262f7bcab2607068743cc187fa3d7cd6c
Ed Butler <edward.butler@ocuco.com>
2020-01-27 14:07:57 +0000

    First pass - dictionary values are valid. Added message on invalid input.

---
+++ RomanNumeralKata/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RomanNumeralKata
+{
+    public class Program
+    {
+        static Dictionary<int, string> numerals = new Dictionary<int, string>
+        {
+            { 1000, "M" },
+            { 900, "CM" },
+            { 500, "D" },
+            { 400, "CD" },
+            { 100, "C" },
+            { 90, "XC" },
+            { 50, "L" },
+            { 40, "XL" },
+            { 10, "X" },
+            { 9, "IX" },
+            { 6, "VI" },
+            { 5, "V" },
+            { 4, "IV" },
+            { 1, "I" }
+        };
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a number for conversion. Arabic numbers will be converted to Roman numerals. Roman numerals will be converted to Arabic numbers.\nYour Input:");
+            var input = Console.ReadLine();
+            var intInput = int.MinValue;
+            if(int.TryParse(input, out intInput))
+            {
+                var stringResult = ArabicToRoman(intInput);
+                if(stringResult != string.Empty)
+                {
+                    Console.WriteLine($"{intInput} in Roman numerals is {stringResult}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please try again.");
+                }
+                
+                Console.ReadKey();
+            } 
+            else
+            {
+                var intResult = RomanToArabic(input);
+                if(intResult != 0)
+                {
+                    Console.WriteLine($"{intInput} in Roman numerals is {intResult}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please try again.");
+                }
+                
+                Console.ReadKey();
+            }
+            
+
+        }
+        public static int RomanToArabic(string romanInput)
+        {
+            var retVal = numerals.FirstOrDefault(n => n.Value == romanInput).Key;
+            return retVal;
+        }
+
+        public static string ArabicToRoman(int arabicInput)
+        {
+            var retVal = numerals[arabicInput];
+            return retVal;
+        }
+    }
+}
---
+++ RomanNumeralKataTest/UnitTest1.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+using RomanNumeralKata;
+namespace RomanNumeralKataTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void OneEqualsI()
+        {
+            Assert.IsTrue(Program.ArabicToRoman(1) == "I");
+        }
+
+        [Test]
+        public void XEqualsTen()
+        {
+            Assert.IsTrue(Program.RomanToArabic("X") == 10);
+        }
+
+        [TestCase("IV", 4)]
+        [TestCase("X", 10)]
+        [TestCase("C", 100)]
+        [TestCase("M", 1000)]
+        public void RomanInputThatExistsInDictionaryIsValid(string input, int expectedValue)
+        {
+            Assert.AreEqual(Program.RomanToArabic(input), expectedValue);
+        }
+
+        [TestCase(5, "V")]
+        [TestCase(40, "XL")]
+        [TestCase(90, "XC")]
+        [TestCase(500, "D")]
+        public void ArabicInputThatExistsInDictionaryIsValid(int input, string expectedValue)
+        {
+            Assert.AreEqual(Program.ArabicToRoman(input), expectedValue);
+        }
+    }
+}
